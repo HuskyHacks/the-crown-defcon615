06b9004d210856881bbe4886fb8585d1da3e078c
husky <huskyhacks.mk@gmail.com>
2022-01-18 20:06:05 -0500

    malware analysis notebook in prog

--- src/pe_parse.py
+++ src/pe_parse.py
@@ -1,22 +1,17 @@
 import pefile
-
-import pefile
-pe = pefile.PE("/home/husky/Desktop/the-crown-defcon615/samples/helloworld/helloworld.exe")
-
 # print(pe.dump_info())
 
 pe.print_info()
 
-
 for entry in pe.OPTIONAL_HEADER.DATA_DIRECTORY:
-    print(entry.name + "\n|\n|---- Size : " + str(entry.Size) + "\n|\n|---- VirutalAddress : " + hex(entry.VirtualAddress) +'\n')
+        print(entry.name + "\n|\n|---- Size : " + str(entry.Size) + "\n|\n|---- VirutalAddress : " + hex(entry.VirtualAddress) +'\n')
 
 print(pe.OPTIONAL_HEADER.AddressOfEntryPoint)
 print(pe.OPTIONAL_HEADER.ImageBase)
 print(pe.FILE_HEADER.NumberOfSections)
 
 for section in pe.sections:
-  print(section.Name, hex(section.VirtualAddress),
+    print(section.Name, hex(section.VirtualAddress),
     hex(section.Misc_VirtualSize), section.SizeOfRawData )
 
 pe.parse_data_directories()
